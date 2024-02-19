52263acf26f19a370b1dba5d3c2dbc2ea049ccf0
piyush254 <76161817+piyush254@users.noreply.github.com>
2024-02-19 15:55:28 +0530

    This is my full code

--- Main.java
+++ Main.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    static class Ronaldo extends Thread {
+        @Override
+        public void run() {
+
+            for (int i = 1; i <= 5; i++) {
+                System.out.println(i + " Ronaldo");
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Ronaldo r = new Ronaldo();
+        r.start();
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(i + " Messi");
+        }
+    }
 }
